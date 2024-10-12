a8538572b210a1cf018ccefec5e495a281396c0e
emmanuel ronoh <eronoh036@gmail.com>
2024-10-12 15:01:58 +0300

    first commit

--- server/models.py
+++ server/models.py
@@ -1,8 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
-
 db = SQLAlchemy()
-
 class Movie(db.Model, SerializerMixin):
     __tablename__ = 'movies'
 
